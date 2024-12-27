08bdca47381183b6d1845e0e80aa1f236451e6ea
KUMARSUNNY <ks9107163@gmail.com>
2024-12-27 10:29:43 +0530

    new project using react

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button"; // Correct Button component import
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero"; // Import the Hero component
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits/>
+        <Collaboration/>
+        <Services/>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
